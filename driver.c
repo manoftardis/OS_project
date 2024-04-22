ef2402573c3ded760381621064aac41518e26bdb
manoftardis <filipposerafini2000@gmail.com>
2024-04-22 07:38:45 +0200

    aggiunti 2 test per mostrare che il threshold di un quarto della page size funziona

--- driver.c
+++ driver.c
@@ -19,18 +19,22 @@ int main(void){
     void* p1= pseudo_malloc(8);
     void* p2= pseudo_malloc(24);
     void* p3= pseudo_malloc(256);
+    void* p6= pseudo_malloc((page_size/4)-1);
 
     void* p4= pseudo_malloc(page_size);
     void* p5= pseudo_malloc(page_size/2);
+    void* p7= pseudo_malloc(page_size/4);
 
     // deallochiamo la memoria
 
     pseudo_free(p1);
     pseudo_free(p2);
     pseudo_free(p3);
+    pseudo_free(p6);
 
     pseudo_free(p4);
     pseudo_free(p5);
+    pseudo_free(p7);
 
     return 1;
 }
